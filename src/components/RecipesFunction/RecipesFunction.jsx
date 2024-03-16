ac1c629713611ddfb7668ea607077e9347cdc04c
samya-roy <samyaroy999@gmail.com>
2024-03-16 23:03:33 +0600

    card funcnality added

--- src/components/RecipesFunction/RecipesFunction.jsx
+++ src/components/RecipesFunction/RecipesFunction.jsx
@@ -1,17 +1,42 @@
+import { useEffect, useState } from "react"
 import Cards from "./Cards/Cards"
 import Tabale from "./Tabale/Tabale"
 
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const RecipesFunction = () => {
+
+  const [cardData, setCardData] = useState([])
+  const [tableData, setTableData] = useState([])
+  useEffect(() => {
+    fetch('./data.json')
+      .then(res => res.json())
+      .then(data => setCardData(data))
+  }, [])
+
+  const hendelCard = (recipeCook) => {
+    const isExgist = tableData.find(item => item.id == recipeCook.id)
+    if (!isExgist) {
+      setTableData([...tableData, recipeCook])
+    } else {
+      toast('alrady exgist')
+    }
+  }
+  console.log(tableData);
+
   return (
-    <div className="grid grid-cols-12">
-      <div className=" col-span-7">
-        <Cards />
+    <>
+      <ToastContainer />
+      <div className="grid grid-cols-12 gap-7">
+        <div className=" col-span-7 grid grid-cols-2 gap-6">
+          {cardData.map(item => <Cards key={item.id} recipe={item} hendelCard={hendelCard} />)}
+        </div>
+        <div className="col-span-5">
+          <Tabale tableData={tableData}/>
+        </div>
       </div>
-      <div className="col-span-5">
-        <Tabale />
-      </div>
-    </div>
+    </>
   )
 }
 
--- src/components/RecipesFunction/Tabale/Tabale.jsx
+++ src/components/RecipesFunction/Tabale/Tabale.jsx
@@ -1,8 +1,14 @@
+import Single from "./singleTabal/Single"
 
 
-const Tabale = () => {
+
+
+const Tabale = ({tableData}) => {
   return (
-    <div>Tabale</div>
+    <div>
+        <h1 className=" font-semibold text-2xl text-center mb-7">Want to cook: 01</h1>
+        <Single tableData={tableData}/>
+    </div>
   )
 }
 
